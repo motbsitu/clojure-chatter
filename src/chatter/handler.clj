eb19a8d6dca2d86aa4934547ac21a2f622decd24
Julie <juliesmike@gmail.com>
2015-09-12 15:29:49 -0500

    finalizing project for web

--- src/chatter/handler.clj
+++ src/chatter/handler.clj
@@ -3,8 +3,11 @@
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [ring.middleware.params :refer [wrap-params]]
+            [ring.adapter.jetty :as jetty]
             [hiccup.page :as page]
-            [hiccup.form :as form]))
+            [hiccup.form :as form]
+            [ring.util.anti-forgery :as anti-forgery]
+            [environ.core :refer [env]]))
 
 (def chat-messages (atom '()))
 
@@ -14,7 +17,10 @@
   [messages]
   (page/html5
    [:head
-    [:title "chatter"]]
+    [:title "chatter"]
+    (page/include-css "//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css")
+    (page/include-js  "//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js")
+    (page/include-css "/chatter.css")]
    [:body
     [:h1 "Our Chat App"]
      [:p
@@ -24,7 +30,7 @@
        "Message: " (form/text-field "msg")
        (form/submit-button "Submit"))]
     [:p
-     [:table
+     [:table#messages.table.table-hover
       (map (fn [m] [:tr [:td (:name m)] [:td (:message m)]]) messages)]]]))
 
 (defn update-messages!
@@ -40,6 +46,11 @@
           new-messages (update-messages! chat-messages name-param msg-param)]
       (generate-message-view new-messages)
       ))
+  (route/resources "/")
   (route/not-found "Not Found"))
 
 (def app (wrap-params app-routes))
+
+(defn -main [& [port]]
+  (let [port (Integer. (or port (env :port) 5000))]
+    (jetty/run-jetty #'app {:port port :join? false})))
