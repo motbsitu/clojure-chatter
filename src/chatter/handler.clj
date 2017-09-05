c056739475239dbccf8acb6cbb8418ce3083b1b9
Julie S. Mike <juliesmike@gmail.com>
2017-09-04 21:23:11 -0500

    test

--- src/chatter/handler.clj
+++ src/chatter/handler.clj
@@ -18,39 +18,39 @@
   (page/html5
    [:head
     [:title "chatter"]
-    (page/include-css "//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css")
-    (page/include-js  "//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js")
-    (page/include-css "/chatter.css")]
-   [:body
-    [:h1 "Our Chat App"]
-     [:p
-      (form/form-to
-       [:post "/"]
-       "Name: " (form/text-field "name")
-       "Message: " (form/text-field "msg")
-       (form/submit-button "Submit"))]
-    [:p
-     [:table#messages.table.table-hover
-      (map (fn [m] [:tr [:td (:name m)] [:td (:message m)]]) messages)]]]))
-
-(defn update-messages!
-  "This will update a message list atom"
-  [messages name new-message]
-  (swap! messages conj {:name name :message new-message}))
-
-(defroutes app-routes
-  (GET "/" [] (generate-message-view @chat-messages))
-  (POST "/" {params :params}
-    (let [name-param (get params "name")
-          msg-param (get params "msg")
-          new-messages (update-messages! chat-messages name-param msg-param)]
-      (generate-message-view new-messages)
-      ))
-  (route/resources "/")
-  (route/not-found "Not Found"))
-
-(def app (wrap-params app-routes))
-
-(defn -main [& [port]]
+;     (page/include-css "//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css")
+;     (page/include-js  "//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js")
+;     (page/include-css "/chatter.css")]
+;    [:body
+;     [:h1 "Our Chat App"]
+;      [:p
+;       (form/form-to
+;        [:post "/"]
+;        "Name: " (form/text-field "name")
+;        "Message: " (form/text-field "msg")
+;        (form/submit-button "Submit"))]
+;     [:p
+;      [:table#messages.table.table-hover
+;       (map (fn [m] [:tr [:td (:name m)] [:td (:message m)]]) messages)]]]))
+;
+; (defn update-messages!
+;   "This will update a message list atom"
+;   [messages name new-message]
+;   (swap! messages conj {:name name :message new-message}))
+;
+; (defroutes app-routes
+;   (GET "/" [] (generate-message-view @chat-messages))
+;   (POST "/" {params :params}
+;     (let [name-param (get params "name")
+;           msg-param (get params "msg")
+;           new-messages (update-messages! chat-messages name-param msg-param)]
+;       (generate-message-view new-messages)
+;       ))
+;   (route/resources "/")
+;   (route/not-found "Not Found"))
+;
+; (def app (wrap-params app-routes))
+;
+; (defn -main [& [port]]
   (let [port (Integer. (or port (env :port) 5000))]
     (jetty/run-jetty #'app {:port port :join? false})))
